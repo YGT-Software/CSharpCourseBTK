03a3f835667638a6ba69aa8996212dae59af3352
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-06-15 08:23:13 +0300

    Entity Framework Demo Project

---
+++ EntityFrameworkDemo/ETradeContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    internal class ETradeContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }// Bu şu demek: Benim bir <Product>'um var onu entity seti olarak yani tablo gibi Products ismiyle kullanacağım. Bu kod veritabanı tablolarında Products diye bir şey arıyor.
+    }
+}
---
+++ EntityFrameworkDemo/ProductDal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityFrameworkDemo
+{
+    public class ProductDal
+    {
+        public List<Product> GetAll()
+        {
+            using (ETradeContext context = new ETradeContext())// using, kod bloğunun işi bittiği anda garbage collector beklenmeden nesneyi zorla bellekten atar.
+            {
+                return context.Products.ToList();//Entity framework'de veritabanındaki tabloya erişmek ve listeye çevirmek...
+            }
+        }
+        public void Add(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                //context.Products.Add(product);
+                var entity = context.Entry(product);
+                entity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);//gönderilen product ile veritabanındaki product ilişkilendiriliyor.
+                entity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void Delete(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
